478d47fb897ab99b3d8612e855d7bed0f18c62e9
Gabriel Magnus <Gabrielsmagnus1@gmail.com>
2026-01-15 21:23:25 -0300

    Condições

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 48
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { /* CONDIÇÃO SIMPLES! */
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log ('Dirija sempre usando cinto de segurança!')
+
+
+
+
+/* FN + ESC + F8 */
