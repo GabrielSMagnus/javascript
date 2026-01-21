606ab659e7757b6f9f396f17a2a403e98c2002e2
Gabriel Magnus <Gabrielsmagnus1@gmail.com>
2026-01-20 23:01:18 -0300

    correção imc

--- desafios/desafioproprio/desafio003/script.js
+++ desafios/desafioproprio/desafio003/script.js
@@ -22,14 +22,19 @@ function rodar() {
 
 
     if (imc < 18.5) {
-        ultimo.innerHTML += `Você está na classificação de magreza.`
+        ultimo.innerHTML = `Você está na classificação de magreza.`
+        return
     } else if (imc >= 18.5 && imc <= 24.9) {
-        ultimo.innerHTML += `Você está na classificação normal.`
+        ultimo.innerHTML = `Você está na classificação normal.`
+        return
     } else if (imc >= 25 && imc <= 29.9) {
-        ultimo.innerHTML += `Você está na clasificação de sobrepeso.`
-    } else if (imc >=30.0 && imc <= 39.9) {
-        ultimo.innerHTML += `Você está na clasificação de obesidade.`
+        ultimo.innerHTML = `Você está na clasificação de sobrepeso.`
+        return
+    } else if (imc >=30.0 && imc <= 39.9){
+        ultimo.innerHTML = `Você está na clasificação de obesidade.`
+        return
     } else if (imc >= 40 ) {
-        ultimo.innerHTML += `Você está na clasificação de obesidade grave.`
+        ultimo.innerHTML = `Você está na clasificação de obesidade grave.`
+        return
     }
 }
